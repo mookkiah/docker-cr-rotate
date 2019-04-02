1eabdce915b350cb7da18aab40c9c847ed323e1c
mogluu <47318409+mogluu@users.noreply.github.com>
2019-04-02 20:02:11 +0000

    Update cr-rotate_k8.py

--- scripts/cr-rotate_k8.py
+++ scripts/cr-rotate_k8.py
@@ -202,16 +202,16 @@ def main():
                        stdout=True, tty=False)
                 stream(cli.connect_get_namespaced_pod_exec, ldap_pods[0].metadata.name, ldap_pods[0].metadata.namespace,
                        command=['/bin/sh', '-c', 'echo ' + str(
-                           server_dn) + '\nchangetype: modify\nreplace: oxTrustCacheRefreshServerIpAddress\n' +
-                                'oxTrustCacheRefreshServerIpAddress: ' + str(
-                           ip) + '\n\n' + str(
-                           server_dn) + '\nchangetype: modify\nreplace: oxTrustConfCacheRefresh\n' + ' >> ' + directory + filename],
+                           server_dn) + ' > ' + directory + filename + '\n echo changetype: modify >> ' + directory + filename +
+                '\n echo replace: oxTrustCacheRefreshServerIpAddress >> ' + directory + filename +
+                                '\n echo oxTrustCacheRefreshServerIpAddress: ' + str(
+                           ip) + ' >> ' + directory + filename + 'echo "/n" >> ' + directory + filename + '\n\n echo ' +
+                                'ou=oxtrust,ou=configuration,' + server_dn + ',ou=appliances,o=gluu' + ' >> ' + directory + filename + '\n echo changetype: modify >> ' + directory + filename +
+                                '\n echo replace: oxTrustConfCacheRefresh' + ' >> ' + directory + filename +
+                                '\n echo ' + cache_refresh_conf + ' >> ' + directory + filename],
                        stderr=True, stdin=False,
                        stdout=True, tty=False)
-                stream(cli.connect_get_namespaced_pod_exec, ldap_pods[0].metadata.name, ldap_pods[0].metadata.namespace,
-                              command=['/bin/sh', '-c', 'echo ' + str(cache_refresh_conf) + ' > ' + directory + filename],
-                              stderr=True, stdin=False,
-                              stdout=True, tty=False)
+
                 ldap_modify_status = stream(cli.connect_get_namespaced_pod_exec, ldap_pods[0].metadata.name, ldap_pods[0].metadata.namespace,
                        command=['/bin/sh', '-c', '/opt/opendj/bin/ldapmodify -D "cn=directory manager" -w "' + bind_password +
                     '" -h localhost -p 1636 --useSSL --trustAll -f ' + directory + filename],
