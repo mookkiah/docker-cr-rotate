a9a891839f386aeddac76fb93712b8ab024306eb
mogluu <47318409+mogluu@users.noreply.github.com>
2019-04-04 19:01:55 +0000

    updated choice exception

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+if [ "$GLUU_CONTAINER_METADATA" = "" ];then
+	printf "Setting enviornment choice to default docker.\nIf you are running Kubernetes please change stop and run with GLUU_CONTAINER_METADATA kubernetes "
+	"$GLUU_CONTAINER_METADATA" = "docker"
+fi
+
 if [ "$GLUU_CONTAINER_METADATA" = "docker" ];then
 	if [ -f /etc/redhat-release ]; then
 		source scl_source enable python27 && python /opt/cr-rotate/scripts/cr_rotating_de.py
