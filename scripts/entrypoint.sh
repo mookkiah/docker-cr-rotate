45dcfa0bce3752b6528985721ea7c60b4ac4abf5
mogluu <47318409+mogluu@users.noreply.github.com>
2019-04-04 18:43:56 +0000

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,8 +2,18 @@
 
 set -e
 
-if [ -f /etc/redhat-release ]; then
-    source scl_source enable python27 && python /opt/cr-rotate/scripts/cr_rotating_de.py
-else
-    python /opt/cr-rotate/scripts/cr_rotating_de.py
+if [ "$GLUU_CONTAINER_METADATA" = "docker" ];then
+	if [ -f /etc/redhat-release ]; then
+		source scl_source enable python27 && python /opt/cr-rotate/scripts/cr_rotating_de.py
+	else
+		python /opt/cr-rotate/scripts/cr_rotating_de.py
+	fi
+fi
+
+if [ "$GLUU_CONTAINER_METADATA" = "kubernetes" ];then
+	if [ -f /etc/redhat-release ]; then
+		source scl_source enable python27 && python /opt/cr-rotate/scripts/cr_rotating_k8.py
+	else
+		python /opt/cr-rotate/scripts/cr_rotating_k8.py
+	fi
 fi
