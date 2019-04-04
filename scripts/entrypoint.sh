22f6ad2af6735282897d3181acefbf8926ec9115
iromli <isman.firmansyah@gmail.com>
2019-04-05 02:55:25 +0700

    show license info

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,6 +2,15 @@
 
 set -e
 
+cat << LICENSE_ACK
+
+# ========================================================================================= #
+# Gluu License Agreement: https://github.com/GluuFederation/gluu-docker/blob/3.1.4/LICENSE. #
+# The use of Gluu Server Docker Edition is subject to the Gluu Support License.             #
+# ========================================================================================= #
+
+LICENSE_ACK
+
 if [ "$GLUU_CONTAINER_METADATA" != "docker" ] && [ "$GLUU_CONTAINER_METADATA" != "kubernetes" ]; then
     echo "Warning: invalid value for GLUU_CONTAINER_METADATA environment variable; fallback to Docker metadata"
     echo ""
