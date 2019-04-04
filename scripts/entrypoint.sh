aa5825fefb8aa1c50443dc8c23aef83c35cd4cbf
iromli <isman.firmansyah@gmail.com>
2019-04-05 03:06:12 +0700

    determine entrypoint for metadata client

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -17,18 +17,17 @@ if [ "$GLUU_CONTAINER_METADATA" != "docker" ] && [ "$GLUU_CONTAINER_METADATA" !=
     GLUU_CONTAINER_METADATA="docker"
 fi
 
-if [ "$GLUU_CONTAINER_METADATA" = "docker" ]; then
-	if [ -f /etc/redhat-release ]; then
-		source scl_source enable python27 && python /opt/cr-rotate/scripts/cr_rotating_de.py
-	else
-		python /opt/cr-rotate/scripts/cr_rotating_de.py
-	fi
-fi
+case $GLUU_CONTAINER_METADATA in
+    "docker")
+        ENTRYPOINT="/opt/cr-rotate/scripts/cr_rotating_de.py"
+        ;;
+    "kubernetes")
+        ENTRYPOINT="/opt/cr-rotate/scripts/cr_rotate_k8.py"
+        ;;
+esac
 
-if [ "$GLUU_CONTAINER_METADATA" = "kubernetes" ]; then
-	if [ -f /etc/redhat-release ]; then
-		source scl_source enable python27 && python /opt/cr-rotate/scripts/cr_rotating_k8.py
-	else
-		python /opt/cr-rotate/scripts/cr_rotating_k8.py
-	fi
+if [ -f /etc/redhat-release ]; then
+    source scl_source enable python27 && python $ENTRYPOINT
+else
+    python $ENTRYPOINT
 fi
