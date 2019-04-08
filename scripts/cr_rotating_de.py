240c8e257db98f0a6a15d811cb8f8426f71af4f8
mogluu <47318409+mogluu@users.noreply.github.com>
2019-04-08 14:56:37 +0000

    Cleaning code. Using LDAP method

--- scripts/cr_rotating_de.py
+++ scripts/cr_rotating_de.py
@@ -9,24 +9,10 @@ import base64
 import docker
 import os
 import pyDes
-import tarfile
 import shutil
 from ldap3 import Server, Connection, MODIFY_REPLACE, MODIFY_ADD, MODIFY_DELETE, SUBTREE, ALL, BASE, LEVEL
 from gluu_config import ConfigManager
 import datetime
-
-
-# Function to copy files from source to destination
-def copy_to(src, container, dst):
-    os.chdir(os.path.dirname(src))
-    srcname = os.path.basename(src)
-    tar = tarfile.open(src + '.tar', mode='w')
-    try:
-        tar.add(srcname)
-    finally:
-        tar.close()
-    data = open(src + '.tar', 'rb').read()
-    container.put_archive(os.path.dirname(dst), data)
 
 
 # Function to decrypt encoded password
@@ -93,52 +79,11 @@ def main():
     # ------- END_Method 2 using consul ----------
     if bind_password:
         try:
-            # Return oxtrust server DN
-            server_dn = ldap_containers[0].exec_run(
-                '/opt/opendj/bin/ldapsearch -h localhost -p 1636 -Z -X -D "cn=directory manager" -w ' + str(
-                    bind_password) + ' -b "ou=appliances,o=gluu"  "inum=*" | grep dn').output.strip()
-            # Return oxtrust conf cache refresh
-            oxtrust_conf_cache_refresh = ldap_containers[0].exec_run('/opt/opendj/bin/ldapsearch -h localhost -p 1636 '
-                                                                     '-Z -X -D "cn=directory manager" -w ' +
-                                                                     str(bind_password) +
-                                                                     ' -b "o=gluu" '
-                                                                     '-T "objectClass=oxTrustConfiguration"'
-                                                                     ' oxTrustCacheRefreshServerIpAddress \ | '
-                                                                     'grep "^oxTrustCacheRefreshServerIpAddress"')\
-                .output.strip()
-            # Get the currently set ip in ldap oxTrustCacheRefreshServerIpAddress
-            current_ip_in_ldap = ldap_containers[0].exec_run('/opt/opendj/bin/ldapsearch -h localhost -p 1636 -Z -X -D '
-                                                             '"cn=directory manager" -w ' + str(bind_password)
-                                                             + ' -b "ou=appliances,o=gluu"  "inum=*" | '
-                                                               'grep "^oxTrustCacheRefreshServerIpAddress"')\
-                .output.strip()
-            current_ip_in_ldap = current_ip_in_ldap[current_ip_in_ldap.find("oxTrustCacheRefreshServerIpAddress: ") +
-                                                    len("oxTrustCacheRefreshServerIpAddress: "):].strip("\n")
-            # From the oxtrust conf cache refresh extract cache refresh conf
-            cache_refresh_conf = oxtrust_conf_cache_refresh[oxtrust_conf_cache_refresh.find("oxTrustConf"
-                                                                                            "CacheRefresh: {"):].strip()
-            # From the oxtrust conf cache refresh extract oxtrust conf cache refresh DN
-            conf_dn = oxtrust_conf_cache_refresh[oxtrust_conf_cache_refresh.find("dn:"):
-                                                 oxtrust_conf_cache_refresh.find("oxTrustConfCacheRefresh")].strip()
-            # Returns an index number if -1 disabled and if => 0 enabled
-            is_cr_enabled = ldap_containers[0].exec_run('/opt/opendj/bin/ldapsearch -h localhost -p 1636 -Z -X -D'
-                                                        ' "cn=directory manager" -w ' + str(bind_password) +
-                                                        ' -b "ou=appliances,o=gluu" "gluuVdsCacheRefreshEnabled=*" '
-                                                        'gluuVdsCacheRefreshEnabled \ | grep -Pzo "enabled"')\
-                .output.find("enabled")
-        except Exception as e:
-            cr_rotating_log.write('[' + str(datetime.datetime.now()) + '] : ' + str(e) + '\n')
-        # ------- Method 2 LDAP -------
-        try:
             # Same structure as above but using LDAP
-            conn_ldap.search('o=gluu', '(objectclass=oxTrustConfiguration)', attributes='oxTrustConfCacheRefresh')
-            oxtrust_conf_cache_refresh_LDAP = str(conn_ldap.entries[0]).strip()
-            cache_refresh_conf_ldap = oxtrust_conf_cache_refresh_LDAP[
-                                 oxtrust_conf_cache_refresh_LDAP.find("oxTrustConfCacheRefresh: "):].strip("\n")
             conn_ldap.search('ou=appliances,o=gluu', '(objectclass=gluuAppliance)', attributes='inum')
             server_dn_LDAP = str(conn_ldap.entries[0]).strip()
             server_dn_ldap = server_dn_LDAP[server_dn_LDAP.find("inum: "):].strip("\n")
-            server_dn_ldap = "inum=" + server_dn_ldap[server_dn_ldap.find("m:") + 3:]
+            server_dn = "inum=" + server_dn_ldap[server_dn_ldap.find("m:") + 3:]
             conn_ldap.search('ou=appliances,o=gluu', '(objectclass=gluuAppliance)',
                              attributes='oxTrustCacheRefreshServerIpAddress')
             current_ip_in_ldap_LDAP = str(conn_ldap.entries[0]).strip()
@@ -167,7 +112,7 @@ def main():
                         shutil.rmtree(directory)
                     except Exception as e:
                         cr_rotating_log.write('[' + str(datetime.datetime.now()) + '] : ' + str(e) + '\n')
-            # Check  the container has not been setup previosly, the CR is enabled
+            # Check  the container has not been setup previously, the CR is enabled
             if ip != current_ip_in_ldap and is_cr_enabled >= 0 and current_ip_in_ldap not in oxtrust_ip_pool:
                 cr_rotating_log.write('[' + str(datetime.datetime.now()) + '] : ' +
                                       str("Current oxTrustCacheRefreshServerIpAddress :  ") +
@@ -175,26 +120,6 @@ def main():
                 cr_rotating_log.write('[' + str(datetime.datetime.now()) + '] : ' +
                                       str("Updating oxTrustCacheRefreshServerIpAddress to ") + str(container.name)
                                       + ' holding id of ' + str(container.id) + ' with ip : ' + str(ip) + '\n')
-                if not os.path.isdir(directory):
-                    try:
-                        os.makedirs(directory)
-                        cr_rotating_log.write('[' + str(datetime.datetime.now()) + '] : ' +
-                                              str('Creating directory : ') + directory + '\n')
-                    except Exception as e:
-                        cr_rotating_log.write('[' + str(datetime.datetime.now()) + '] : ' + str(e) + '\n')
-                # Clear contents of file at CR rotate container
-                open(directory + filename, 'w+').close()
-                cr_rotating_log.write('[' + str(datetime.datetime.now()) + '] : ' +
-                                      str('Writing LDIF to  : ') + directory + filename + '\n')
-                # Format and concatenate ldifdata
-                ldifdata = str(
-                    server_dn) + "\nchangetype: modify\nreplace: oxTrustCacheRefreshServerIpAddress\n" \
-                                 "oxTrustCacheRefreshServerIpAddress: " + str(
-                    ip) + "\n\n" + str(conf_dn) + "\nchangetype: modify\nreplace: oxTrustConfCacheRefresh\n" + str(
-                    cache_refresh_conf)
-                ldif = open(directory + filename, "w+")
-                ldif.write(ldifdata)
-                ldif.close()
                 cr_rotating_log.write('[' + str(datetime.datetime.now()) + '] : ' +
                                       str("Cleaning cache folderes for  ") + str(container.name)
                                       + ' holding id of ' + str(container.id) + ' with ip : ' + str(ip) + '\n')
@@ -202,33 +127,12 @@ def main():
                 container.exec_run('rm -rf /var/ox/identity/cr-snapshots/')
                 container.exec_run('mkdir /var/ox/identity/cr-snapshots/')
                 container.exec_run('chown -R jetty:jetty /var/ox/identity/cr-snapshots/')
-                cr_rotating_log.write('[' + str(datetime.datetime.now()) + '] : ' +
-                                      str('Creating file  : ') + directory + filename + ' at ' +
-                                      str(ldap_containers[0].name) + ' holding id of ' +
-                                      str(ldap_containers[0].id) + '\n')
-                ldap_containers[0].exec_run(' mkdir -p ' + directory)
-                cr_rotating_log.write('[' + str(datetime.datetime.now()) + '] : ' +
-                                      str('Copying LDIF to  : ') + str(ldap_pods[0].name) + '\n')
-                copy_to(directory + filename, ldap_containers[0], directory + filename)
-                ldap_modify_status = ldap_containers[0].exec_run(
-                    '/opt/opendj/bin/ldapmodify -D "cn=directory manager" -w ' + bind_password +
-                    ' -h localhost -p 1636 --useSSL --trustAll -f ' + directory + filename).output
-                cr_rotating_log.write('[' + str(datetime.datetime.now()) + '] : ' + str(ldap_modify_status) + '\n')
-                # Clean up files
-                cr_rotating_log.write('[' + str(datetime.datetime.now()) + '] : ' +
-                                      str('Cleaning files at  : ') + str(ldap_pods[0].name) + '\n')
-                ldap_containers[0].exec_run('rm -rf ' + directory + filename)
-                # ------- Method 2 LDAP -------
                 try:
-                    conn_ldap.modify(server_dn_ldap + ',ou=appliances,o=gluu',
+                    conn_ldap.modify(server_dn + ',ou=appliances,o=gluu',
                                      {'oxTrustCacheRefreshServerIpAddress': [(MODIFY_REPLACE, [ip])]})
-                    cr_rotating_log.write('[' + str(datetime.datetime.now()) + '] : ' + str(conn_ldap.result) + '\n')
-                    conn_ldap.modify('ou=oxtrust,ou=configuration,' + server_dn_ldap + ',ou=appliances,o=gluu',
-                                     {'oxTrustConfCacheRefresh': [(MODIFY_REPLACE, [cache_refresh_conf_ldap])]})
                     cr_rotating_log.write('[' + str(datetime.datetime.now()) + '] : ' + str(conn_ldap.result) + '\n')
                 except Exception as e:
                     cr_rotating_log.write('[' + str(datetime.datetime.now()) + '] : ' + str(e) + '\n')
-                # ------- END_Method 2 LDAP -------
 # ------------------------------------
 if __name__ == "__main__":
     main()
--- scripts/cr_rotating_k8.py
+++ scripts/cr_rotating_k8.py
@@ -109,68 +109,11 @@ def main():
     # ------- END_Method 2 using consul ----------
     if bind_password:
         try:
-            # Return oxtrust server DN
-            server_dn = stream(cli.connect_get_namespaced_pod_exec, ldap_pods[0].metadata.name,
-                               ldap_pods[0].metadata.namespace,
-                               command=['/bin/sh', '-c', '/opt/opendj/bin/ldapsearch -h '
-                                                         'localhost -p 1636 -Z -X -D "cn=directory manager" -w "' +
-                                        str(bind_password) + '" -b "ou=appliances,o=gluu"  "inum=*" | grep dn'],
-                               stderr=True, stdin=True, stdout=True, tty=False).split()
-            server_dn = ''.join(server_dn).strip()
-            # Return oxtrust conf cache refresh
-            oxtrust_conf_cache_refresh = stream(cli.connect_get_namespaced_pod_exec, ldap_pods[0].metadata.name,
-                                                ldap_pods[0].metadata.namespace,
-                                                command=['/bin/sh', '-c', '/opt/opendj/bin/ldapsearch -h '
-                                                                          'localhost -p 1636 -Z -X -D '
-                                                                          '"cn=directory manager" -w "' +
-                                                         str(bind_password) + '" -b "o=gluu" -T '
-                                                                              '"objectClass=oxTrustConfiguration" '
-                                                                              'oxTrustConfCacheRefresh \ | '
-                                                                              'grep "^oxTrustConfCacheRefresh"'],
-                                                stderr=True, stdin=True, stdout=True, tty=False).split()
-            oxtrust_conf_cache_refresh = ''.join(oxtrust_conf_cache_refresh).strip()
-            # get current oxTrustCacheRefreshServerIpAddress
-            current_ip_in_ldap = stream(cli.connect_get_namespaced_pod_exec, ldap_pods[0].metadata.name,
-                                        ldap_pods[0].metadata.namespace,
-                                        command=['/bin/sh', '-c','/opt/opendj/bin/ldapsearch -h localhost -p 1636 -Z -X'
-                                                                 ' -D "cn=directory manager" -w ' + str(bind_password) +
-                                                 ' -b "ou=appliances,o=gluu" "inum=*" | grep'
-                                                 ' "^oxTrustCacheRefreshServerIpAddress"'],
-                                        stderr=True, stdin=True, stdout=True, tty=False).split()
-            current_ip_in_ldap = ''.join(current_ip_in_ldap).strip()
-            current_ip_in_ldap = current_ip_in_ldap[
-                                 current_ip_in_ldap.find("oxTrustCacheRefreshServerIpAddress: ") + len(
-                                     "oxTrustCacheRefreshServerIpAddress: "):].strip("\n")
-            # From the oxtrust conf cache refresh extract cache refresh conf
-            cache_refresh_conf = oxtrust_conf_cache_refresh[oxtrust_conf_cache_refresh.find("oxTrustConfCacheRefresh:"
-                                                                                            ):].strip()
-            # From the oxtrust conf cache refresh extract oxtrust conf cache refresh DN
-            conf_dn = oxtrust_conf_cache_refresh[oxtrust_conf_cache_refresh.find("dn:"):oxtrust_conf_cache_refresh.find(
-                "oxTrustConfCacheRefresh")].strip()
-            # Returns an index number if -1 disabled and if => 0 enabled
-            is_cr_enabled = stream(cli.connect_get_namespaced_pod_exec, ldap_pods[0].metadata.name,
-                                   ldap_pods[0].metadata.namespace,
-                                   command=['/bin/sh', '-c', '/opt/opendj/bin/ldapsearch -h localhost -p 1636 -Z -X -D '
-                                                             '"cn=directory manager" -w "' + str(bind_password) +
-                                            '" -b "ou=appliances,o=gluu" "gluuVdsCacheRefreshEnabled=*" '
-                                            'gluuVdsCacheRefreshEnabled \ | grep -Pzo "enabled"'],
-                                   stderr=True, stdin=True, stdout=True, tty=False).find("enabled")
-            # From the oxtrust conf cache refresh extract cache refresh conf
-            cache_refresh_conf = oxtrust_conf_cache_refresh[oxtrust_conf_cache_refresh.find("oxTrustConfCacheRefresh:")
-                                                            :].strip()
-        except Exception as e:
-            cr_rotating_log.write('[' + str(datetime.datetime.now()) + '] : ' + str(e) + '\n')
-        # ------- Method 2 LDAP -------
-        try:
             # Same structure as above but using LDAP
-            conn_ldap.search('o=gluu', '(objectclass=oxTrustConfiguration)', attributes='oxTrustConfCacheRefresh')
-            oxtrust_conf_cache_refresh_LDAP = str(conn_ldap.entries[0]).strip()
-            cache_refresh_conf_ldap = oxtrust_conf_cache_refresh_LDAP[
-                                      oxtrust_conf_cache_refresh_LDAP.find("oxTrustConfCacheRefresh: "):].strip("\n")
             conn_ldap.search('ou=appliances,o=gluu', '(objectclass=gluuAppliance)', attributes='inum')
             server_dn_LDAP = str(conn_ldap.entries[0]).strip()
             server_dn_ldap = server_dn_LDAP[server_dn_LDAP.find("inum: "):].strip("\n")
-            server_dn_ldap = "inum=" + server_dn_ldap[server_dn_ldap.find("m:") + 3:]
+            server_dn = "inum=" + server_dn_ldap[server_dn_ldap.find("m:") + 3:]
             conn_ldap.search('ou=appliances,o=gluu', '(objectclass=gluuAppliance)',
                              attributes='oxTrustCacheRefreshServerIpAddress')
             current_ip_in_ldap_LDAP = str(conn_ldap.entries[0]).strip()
@@ -185,19 +128,13 @@ def main():
                 "\n").find("enabled")
         except Exception as e:
             cr_rotating_log.write('[' + str(datetime.datetime.now()) + '] : ' + str(e) + '\n')
-        # ------- END_Method 2 LDAP -------
         for oxtrust_pod in oxtrust_pods:
             ip = oxtrust_pod.status.pod_ip
             # The user has disabled the CR or CR is not active
             if is_cr_enabled < 0:
                 cr_rotating_log.write('[' + str(datetime.datetime.now()) + '] : ' +
                                         str('Cache refresh is found to be disabled. Cleaning files...') + '\n')
-                # Check if the path for the LDIF exists and if so remove it
-                if os.path.isdir(directory):
-                    try:
-                        shutil.rmtree(directory)
-                    except Exception as e:
-                        cr_rotating_log.write('[' + str(datetime.datetime.now()) + '] : ' + str(e) + '\n')
+
             # Check  is the current ip of oxtrust pod is not the same as the one in oxTrustCacheRefreshServerIpAddress
             # Check is CR is Enabled
             # Check that the oxTrustCacheRefreshServerIpAddress is not in the oxtrust_ip_pool
@@ -208,28 +145,8 @@ def main():
                 cr_rotating_log.write('[' + str(datetime.datetime.now()) + '] : ' +
                                       str("Updating oxTrustCacheRefreshServerIpAddress to ") +
                                       str(oxtrust_pod.metadata.name) + ' with ip : ' + str(ip) + '\n')
-                if not os.path.isdir(directory):
-                    try:
-                        os.makedirs(directory)
-                        cr_rotating_log.write('[' + str(datetime.datetime.now()) + '] : ' +
-                                                str('Creating directory : ') + directory + '\n')
-                    except Exception as e:
-                        cr_rotating_log.write('[' + str(datetime.datetime.now()) + '] : ' + str(e) + '\n')
 
-                # Clear contents of file at CR rotate pod
-                open(directory + filename, 'w+').close()
-                # Format and concatenate LDIF data
-                ldifdata = str(server_dn) + '\nchangetype: modify\nreplace: oxTrustCacheRefreshServerIpAddress\n' \
-                                            'oxTrustCacheRefreshServerIpAddress: ' + str(ip) + '\n\n' + str(server_dn) \
-                           + '\nchangetype: modify\nreplace: oxTrustConfCacheRefresh\n' + str(cache_refresh_conf)
-                ldif = open(directory + filename, "w+")
-                cr_rotating_log.write('[' + str(datetime.datetime.now()) + '] : ' +
-                                        str('Writing LDIF to  : ') + directory + filename + '\n')
-                ldif.write(ldifdata)
-                ldif.close()
-                cr_rotating_log.write('[' + str(datetime.datetime.now()) + '] : ' +
-                                        str("Cleaning cache folders for ") + str(oxtrust_pod.metadata.name)
-                                        + ' with ip : ' + str(ip) + '\n')
+
                 # Clean cache folder at oxtrust pod
                 stream(cli.connect_get_namespaced_pod_exec, oxtrust_pod.metadata.name, oxtrust_pod.metadata.namespace,
                        command=['/bin/sh','-c','rm -rf /var/ox/identity/cr-snapshots'],
@@ -250,47 +167,12 @@ def main():
                        command=['/bin/sh','-c','mkdir -p ' + directory],
                        stderr=True, stdin=True,
                        stdout=True, tty=False)
-                cr_rotating_log.write('[' + str(datetime.datetime.now()) + '] : ' +
-                                        str('Writing LDIF to  : ') + str(ldap_pods[0].metadata.name) + '\n')
-                stream(cli.connect_get_namespaced_pod_exec, ldap_pods[0].metadata.name, ldap_pods[0].metadata.namespace,
-                       command=['/bin/sh', '-c', 'echo ' + str(server_dn) + ' > ' + directory + filename +
-                                '\n echo changetype: modify >> ' + directory + filename +
-                                '\n echo replace: oxTrustCacheRefreshServerIpAddress >> ' + directory + filename +
-                                '\n echo oxTrustCacheRefreshServerIpAddress: ' + str(ip) + ' >> ' +
-                                directory + filename + 'echo "\n" >> ' + directory + filename + '\n\n echo ' +
-                                'dn: ou=oxtrust,ou=configuration,' + server_dn[server_dn.find('dn:') + 3:] + ' >> ' +
-                                directory + filename + '\n echo changetype: modify >> ' + directory + filename +
-                                '\n echo replace: oxTrustConfCacheRefresh' + ' >> ' + directory + filename +
-                                '\n echo ' + cache_refresh_conf + ' >> ' + directory + filename],
-                       stderr=True, stdin=False,
-                       stdout=True, tty=False)
-
-                ldap_modify_status = stream(cli.connect_get_namespaced_pod_exec, ldap_pods[0].metadata.name,
-                                            ldap_pods[0].metadata.namespace,
-                                            command=['/bin/sh', '-c',
-                                                     '/opt/opendj/bin/ldapmodify -D "cn=directory manager" -w "' +
-                                                     bind_password +'" -h localhost -p 1636 --useSSL --trustAll -f ' +
-                                                     directory + filename],
-                                            stderr=True, stdin=True, stdout=True, tty=False)
-
-                cr_rotating_log.write('[' + str(datetime.datetime.now()) + '] : ' + str(ldap_modify_status) + '\n')
-                # Clean up files
-                cr_rotating_log.write('[' + str(datetime.datetime.now()) + '] : ' +
-                                        str('Cleaning files at  : ') + str(ldap_pods[0].metadata.name) + '\n')
-                stream(cli.connect_get_namespaced_pod_exec, ldap_pods[0].metadata.name, ldap_pods[0].metadata.namespace,
-                       command=['/bin/sh','-c', 'rm -rf ' + directory + filename],
-                       stderr=True, stdin=True, stdout=True, tty=False)
-                # ------- Method 2 LDAP -------
                 try:
                     conn_ldap.modify(server_dn_ldap + ',ou=appliances,o=gluu',
                                 {'oxTrustCacheRefreshServerIpAddress': [(MODIFY_REPLACE, [ip])]})
                     cr_rotating_log.write('[' + str(datetime.datetime.now()) + '] : ' + str(conn_ldap.result) + '\n')
-                    conn_ldap.modify('ou=oxtrust,ou=configuration,' + server_dn_ldap + ',ou=appliances,o=gluu',
-                                {'oxTrustConfCacheRefresh': [(MODIFY_REPLACE, [cache_refresh_conf_ldap])]})
-                    cr_rotating_log.write('[' + str(datetime.datetime.now()) + '] : ' + str(conn_ldap.result) + '\n')
                 except Exception as e:
                     cr_rotating_log.write('[' + str(datetime.datetime.now()) + '] : ' + str(e) + '\n')
-                # ------- END_Method 2 LDAP -------
 # ------------------------------------
 
 
